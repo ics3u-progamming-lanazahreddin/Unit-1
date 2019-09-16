d070dbc166bbcdcfacac5339f1d3bc5bb772f6f3
LanaZahrEddin <43419131+LanaZahrEddin@users.noreply.github.com>
2019-09-16 12:54:48 -0400

    added screenshots and comments in the code

--- SportsLanaZE/SportsLanaZE/SportsForm.cs
+++ SportsLanaZE/SportsLanaZE/SportsForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Lana ZE
+ * Created on: 14-09-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #7 - Sports
+ * This program displays 4 sports with my favourite player of each sport
+ * and a picture each the player in the background
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,19 +22,14 @@ namespace SportsLanaZE
     {
         public frmSpots()
         {
+            // this changes the background image of the entire form
             this.BackgroundImage = Properties.Resources.sports;
             InitializeComponent();
         }
 
-        private void SoccerToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.BackgroundImage = Properties.Resources.soccerplayer;
-            this.lblSportType.Text = "Soccer";
-            this.lblFavPlayer.Text = "Christiano ronaldo";
-        }
-
         private void MniSports_Click(object sender, EventArgs e)
         {
+            //this label displays sports when you click on it with a background image
             this.lblSportType.Text = "Sports :)";
             this.lblFavPlayer.Text = "";
         }
@@ -36,8 +39,21 @@ namespace SportsLanaZE
 
         }
 
+        private void SoccerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //this label displays my fav soccer player with an image of him
+            // //when you click on it
+            this.BackgroundImage = Properties.Resources.soccerplayer;
+            this.lblSportType.Text = "Soccer";
+            this.lblFavPlayer.Text = "Christiano ronaldo";
+        }
+
+       
+
         private void MniTennis_Click(object sender, EventArgs e)
         {
+            //this label displays my fav tennis player with a picture of her
+            // //when you click on it
             this.BackgroundImage = Properties.Resources.tennisplayer;
             this.lblSportType.Text = "Tennis";
             this.lblFavPlayer.Text = "Bianca Andreescu";
@@ -45,6 +61,8 @@ namespace SportsLanaZE
 
         private void MniBasketball_Click(object sender, EventArgs e)
         {
+            //this displays my fav basketball player and a picture of him 
+            //when you click on it
             this.BackgroundImage = Properties.Resources.basketballplayer;
             this.lblSportType.Text = "Basketball";
             this.lblFavPlayer.Text = "LeBron James";
@@ -52,6 +70,8 @@ namespace SportsLanaZE
 
         private void MniFootball_Click(object sender, EventArgs e)
         {
+            //this label displays my fav football player with a picture of him
+            //when u click on it
             this.BackgroundImage = Properties.Resources.footballplayer;
             this.lblSportType.Text = "Football";
             this.lblFavPlayer.Text = "Odell Beckham Jr";
