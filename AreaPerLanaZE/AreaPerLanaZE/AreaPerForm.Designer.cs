6ca389fb4d4cfc2a870d20586c92dfb6c2b66529
LanaZahrEddin <43419131+LanaZahrEddin@users.noreply.github.com>
2019-09-17 13:13:37 -0400

    added area perimeter program

---
+++ AreaPerLanaZE/AreaPerLanaZE/AreaPerForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace AreaPerLanaZE
+{
+    partial class frmAreaPer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblPer = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblPerAnswer = new System.Windows.Forms.Label();
+            this.lblAreaAnswer = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLength.Location = new System.Drawing.Point(79, 81);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(214, 52);
+            this.lblLength.TabIndex = 2;
+            this.lblLength.Text = "Length (m)";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWidth.Location = new System.Drawing.Point(79, 184);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(204, 52);
+            this.lblWidth.TabIndex = 3;
+            this.lblWidth.Text = "Width (m)";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(403, 96);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(159, 20);
+            this.txtLength.TabIndex = 4;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(403, 211);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(159, 20);
+            this.txtWidth.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(616, 136);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(161, 53);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblPer
+            // 
+            this.lblPer.AutoSize = true;
+            this.lblPer.Font = new System.Drawing.Font("ISOCPEUR", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPer.Location = new System.Drawing.Point(177, 299);
+            this.lblPer.Name = "lblPer";
+            this.lblPer.Size = new System.Drawing.Size(183, 31);
+            this.lblPer.TabIndex = 7;
+            this.lblPer.Text = "The perimeter is:";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Font = new System.Drawing.Font("ISOCPEUR", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(177, 368);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(133, 31);
+            this.lblArea.TabIndex = 8;
+            this.lblArea.Text = "The area is:";
+            // 
+            // lblPerAnswer
+            // 
+            this.lblPerAnswer.AutoSize = true;
+            this.lblPerAnswer.Font = new System.Drawing.Font("AcadEref", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPerAnswer.Location = new System.Drawing.Point(523, 304);
+            this.lblPerAnswer.Name = "lblPerAnswer";
+            this.lblPerAnswer.Size = new System.Drawing.Size(27, 20);
+            this.lblPerAnswer.TabIndex = 9;
+            this.lblPerAnswer.Text = "??";
+            // 
+            // lblAreaAnswer
+            // 
+            this.lblAreaAnswer.AutoSize = true;
+            this.lblAreaAnswer.Font = new System.Drawing.Font("AcadEref", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAreaAnswer.Location = new System.Drawing.Point(523, 373);
+            this.lblAreaAnswer.Name = "lblAreaAnswer";
+            this.lblAreaAnswer.Size = new System.Drawing.Size(27, 20);
+            this.lblAreaAnswer.TabIndex = 10;
+            this.lblAreaAnswer.Text = "??";
+            // 
+            // frmAreaPer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAreaAnswer);
+            this.Controls.Add(this.lblPerAnswer);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblPer);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmAreaPer";
+            this.Text = "Area and Perimeter of Rectangle by Lana ZE";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblPer;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerAnswer;
+        private System.Windows.Forms.Label lblAreaAnswer;
+    }
+}
+
---
+++ AreaPerLanaZE/AreaPerLanaZE/AreaPerForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaPerLanaZE
+{
+    public partial class frmAreaPer : Form
+    {
+        public frmAreaPer()
+        {
+            InitializeComponent();
+
+            //hide the area and perimeter answer labels until the user hits the calculate button
+            this.lblPer.Hide();
+            this.lblArea.Hide();
+            this.lblPerAnswer.Hide();
+            this.lblAreaAnswer.Hide();
+        }
+
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            double length, width, area, perimeter;
+
+
+            // convert the string from each text box to a double
+            length = double.Parse(txtLength.Text);
+            width = double.Parse(txtWidth.Text);
+
+            // Calculate the area and the perimeter
+            area = width * length;
+            perimeter = 2 * (width + length);
+
+            // insert the area and perimeter into their respective variables
+            this.lblPerAnswer.Text = Convert.ToString(perimeter) + "  meters";
+            this.lblAreaAnswer.Text = Convert.ToString(area) + " squared meters";
+
+            // Show the labels back on the screen when the calculate button is hit
+            this.lblPerAnswer.Show();
+            this.lblAreaAnswer.Show();
+        }
+    }
+}
