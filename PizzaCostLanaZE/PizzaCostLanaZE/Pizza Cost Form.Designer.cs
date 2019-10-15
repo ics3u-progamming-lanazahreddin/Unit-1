eceb9ad299c380d163893cfa3b99f8c9b8c306d6
lanazahreddin81 <47094765+lanazahreddin81@users.noreply.github.com>
2019-10-14 21:03:59 -0400

    added screenshots for moving cat

--- PizzaCostLanaZE/PizzaCostLanaZE/Pizza Cost Form.Designer.cs
+++ PizzaCostLanaZE/PizzaCostLanaZE/Pizza Cost Form.Designer.cs
@@ -28,20 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblText = new System.Windows.Forms.Label();
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAnswerText = new System.Windows.Forms.Label();
+            this.lblCost = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Yu Gothic UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(34, 57);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(670, 46);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Enter the diameter of the pizza (in inches):";
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.Location = new System.Drawing.Point(777, 77);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(140, 22);
+            this.txtDiameter.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalculate.Font = new System.Drawing.Font("Yu Gothic UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(298, 188);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(420, 128);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblAnswerText
+            // 
+            this.lblAnswerText.AutoSize = true;
+            this.lblAnswerText.Font = new System.Drawing.Font("Yu Gothic UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerText.Location = new System.Drawing.Point(34, 405);
+            this.lblAnswerText.Name = "lblAnswerText";
+            this.lblAnswerText.Size = new System.Drawing.Size(704, 46);
+            this.lblAnswerText.TabIndex = 3;
+            this.lblAnswerText.Text = "Here is the cost of your pizza (including tax):";
+            // 
+            // lblCost
+            // 
+            this.lblCost.AutoSize = true;
+            this.lblCost.Font = new System.Drawing.Font("Yu Gothic UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCost.ForeColor = System.Drawing.Color.Brown;
+            this.lblCost.Location = new System.Drawing.Point(796, 413);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(41, 38);
+            this.lblCost.TabIndex = 4;
+            this.lblCost.Text = "??";
             // 
             // frmPizzaCost
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.lblAnswerText);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtDiameter);
+            this.Controls.Add(this.lblText);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmPizzaCost";
             this.Text = "Pizza Cost";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAnswerText;
+        private System.Windows.Forms.Label lblCost;
     }
 }
 
--- PizzaCostLanaZE/PizzaCostLanaZE/Pizza Cost Form.cs
+++ PizzaCostLanaZE/PizzaCostLanaZE/Pizza Cost Form.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Lana ZE
+ * Created on: September 14, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #12 - Pseudocode
+ * This program calculates the cost of a pizza when given the diameter of the pizza (in inches)
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +22,24 @@ namespace PizzaCostLanaZE
         public frmPizzaCost()
         {
             InitializeComponent();
+            // hide the cost label
+            lblCost.Hide();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            // Variables
+            double costBeforeTax, costAfterTax;
+            int diameter;
+
+            // convert the diameter into a string
+            diameter = Convert.ToInt32(txtDiameter.Text);
+
+            // calculation for the pizza cost after tax
+            costBeforeTax = 0.75 + 0.99 + 0.5 * diameter;
+            costAfterTax = costBeforeTax * 1.13;
+            lblCost.Text = "$" + Convert.ToString(Math.Round(costAfterTax , 2));
+            lblCost.Show();
         }
     }
 }
